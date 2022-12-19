d170d87a17c3a50f7d4ddb3a0f58cc7f51574567
Marc Bertran <mbertransuarez@gmail.com>
2022-12-19 16:48:48 +0100

    lab done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true);
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -17,7 +17,22 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({title: 'Pizza', level: 'Easy Peasy', ingredients: ['mozzarella', 'tomato', 'mushrooms'], cuisine: 'Italian', dishType: 'main_course', duration: 30, creator: 'Marc'})
   })
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then(data => console.log(data[0].title, data[1].title, data[2].title, data[3].title, data[4].title))
+  //createdRecipe => console.log(createdRecipe.title)
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+  })
+  .then(recipeUpdated => console.log(`${recipeUpdated} Updated successfully!`))
+  .then(() => {
+    return Recipe.deleteOne({title: 'Chocolate Chip Cookies'})
+  })
+  .then(deletedRecipe => console.log(`${deletedRecipe} deleted`))
+  .then(() => mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,47 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    unique: true,
+    required: [true, 'Title is mandatory']
+  },
+  level: {
+    type: String,
+    enum: {
+      values: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+      message: 'That is not an accepted value. Please try again.'
+    }
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String,
+    required: [true, 'Cuisine is mandatory']
+  },
+  dishType: {
+    type: String,
+    enum: {
+      values: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'],
+      message: 'That is not an accepted value. Please try again'
+    }
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
